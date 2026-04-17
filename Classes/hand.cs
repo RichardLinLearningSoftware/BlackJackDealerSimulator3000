c5a926d6e38521588c62e939faaa0374a547b2c6
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2026-04-17 13:53:18 +0200

    Small commit
    -Added more content for the Hand/player class
    
    -I still need a way to visualize the players

--- Classes/hand.cs
+++ Classes/hand.cs
@@ -7,6 +7,7 @@ namespace BlackJackDealerSimulator3000.Classes
 {
     internal class Hand
     {
+        private String playerName;
         ArrayList hand = new ArrayList();
 
         public void AddCard( Card card, bool flipCard = false)
@@ -16,6 +17,21 @@ namespace BlackJackDealerSimulator3000.Classes
                 card.Flip();
             }
             hand.Add(card);
+        }
+        public Hand(String playerName)
+        {
+            this.playerName = playerName;
+        }
+
+        public string StandOrHit()
+        {
+            Random random = new Random();
+            int randomInt = random.Next(0, 2);
+            if(randomInt == 0)
+            {
+                return "I want to Stand";
+            }
+            return "I want to Hit";
         }
     }
 }
